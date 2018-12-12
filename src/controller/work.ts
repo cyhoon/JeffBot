af6cfa04b66ef732aaf6126309acee0cec4cf524
ihello72 <ihello0720@gmail.com>
2018-12-12 09:08:08 +0900

    업무 시간 예약 기능 추가

--- src/controller/work.ts
+++ src/controller/work.ts
@@ -1,10 +1,26 @@
+import * as moment from 'moment';
+
 import bot from './../lib/bot';
 
 const { MY_TELEGRAM_ID } = process.env;
 
 // 1시간에 한 번씩
 let botAlert;
+let reservationBotAlert;
 let hour = 0;
+
+const startBotAlert = (startMillisecond) => {
+  botAlert = setInterval(() => {
+    hour += 1;
+    bot.sendMessage(MY_TELEGRAM_ID, `${hour}시간째 일하고 계시네요!`);
+  }, startMillisecond);
+};
+
+const endBotAlert = () => {
+  clearInterval(botAlert);
+  clearInterval(reservationBotAlert);
+  bot.sendMessage(MY_TELEGRAM_ID, `총 ${hour}시간 일했습니다!`);
+}
 
 const startWork = (msg) => {
   const chatId: number = msg.chat.id;
@@ -13,10 +29,7 @@ const startWork = (msg) => {
     return;
   }
 
-  botAlert = setInterval(() => {
-    hour += 1;
-    bot.sendMessage(MY_TELEGRAM_ID, `${hour}시간째 일하고 계시네요!`);
-  }, 3600000);
+  startBotAlert(3600000);
 
   bot.sendMessage(chatId, '일을 시작합니다');
 };
@@ -26,9 +39,30 @@ const endWork = (msg) => {
 
   if (chatId.toString() !== MY_TELEGRAM_ID) return;
 
-  clearInterval(botAlert);
-
-  bot.sendMessage(chatId, `총 ${hour}시간 일했습니다!`);
+  endBotAlert();
 };
 
-export { startWork, endWork };
+const startWorkByHour = (msg, match) => {
+  const workStartHour = match[1];
+
+  const nowTime: number = moment().valueOf();
+  const workStartTime: number = moment().set({ h: workStartHour, m: 0, s: 0}).valueOf();
+
+  const startMillisecond: number = workStartTime - nowTime;
+
+  if (startMillisecond < 0) {
+    bot.sendMessage(MY_TELEGRAM_ID, '지금은 일하지마세요!');
+    return;
+  }
+
+  reservationBotAlert = setTimeout(() => {
+    bot.sendMessage(MY_TELEGRAM_ID, `업무를 시작합니다`);
+    startBotAlert(3600000);
+  }, startMillisecond);
+
+  const startTime = moment(workStartTime).format('YYYY-MM-DD HH:00:00');
+
+  bot.sendMessage(msg.chat.id, `${startTime} 시간에 업무를 시작합니다`);
+};
+
+export { startWork, endWork, startWorkByHour };
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ require('dotenv').config();
 import bot from './lib/bot';
 import * as work from './controller/work';
 
-bot.onText(/\/work\/start/, work.startWork);
-bot.onText(/\/work\/end/, work.endWork);
+bot.onText(/\/work\/start$/, work.startWork);
+bot.onText(/\/work\/end$/, work.endWork);
+bot.onText(/\/work\/start (.+)/, work.startWorkByHour);
