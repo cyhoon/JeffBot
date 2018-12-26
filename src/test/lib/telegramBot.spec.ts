801d5fdf99f6e7f66b9e993757cdbe4eca1350d0
jeffchoi <ihello0720@gmail.com>
2018-12-27 08:59:05 +0900

    테스트 코드에 .env 환경 변수 추가 ( TELEGRAM_BOT_TOKEN )

--- src/test/lib/telegramBot.spec.ts
+++ src/test/lib/telegramBot.spec.ts
@@ -1,11 +1,11 @@
 require('dotenv').config();
+process.env.TELEGRAM_BOT_TOKEN = "708176528:AAGXLXT1YQY2h8nYGmCzJspjmDxuPgXcfHc";
 
 import { describe, it } from 'mocha';
 import { expect, assert } from 'chai';
 
 import * as TelegramBotApi from 'node-telegram-bot-api';
 import TelegramBot from '../../lib/telegramBot';
-const { TELEGRAM_BOT_TOKEN } = process.env;
 
 describe('telegramBot', () => {
   it ('isReturningTelegramInstance', () => {
