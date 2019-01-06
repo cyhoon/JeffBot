7e96cdb2efb2d9c6af21dea379c8097ab5f08861
jeffchoi <ihello0720@gmail.com>
2019-01-06 23:10:14 +0900

    Update execute-deploy.sh

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/home/ubuntu/docker-image/jeff-bot/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+cd /home/ubuntu/docker-images/jeff-bot
+./deploy.sh > /dev/null 2> /dev/null < /dev/null &
